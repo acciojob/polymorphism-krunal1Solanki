15fc32010143c41e8a89728114b209f12cb26d97
krunalSolanki1 <krunalsolucky121@gmail.com>
2023-01-12 22:46:12 +0530

    s

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
-
+class Product {
+    public int product(int x, int y) {
+        return 0;
+    }
+    public int product(int x, int y, int z) {
+        return 0;
+    }
+    public double product(double x, double y) {
+        return 0;
+    }
+}
 public class Main {
-
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.2, 2.3);
+    }
 }
