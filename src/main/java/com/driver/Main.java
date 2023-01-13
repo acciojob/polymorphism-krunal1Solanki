ae5f973eb56d54c04a12650386f5581f927b057f
krunalSolanki1 <krunalsolucky121@gmail.com>
2023-01-13 14:45:30 +0530

    s

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,17 @@
 package com.driver;
-class Product {
-    public int product(int x, int y) {
-        return 0;
-    }
-    public int product(int x, int y, int z) {
-        return 0;
-    }
-    public double product(double x, double y) {
-        return 0;
-    }
-}
+
 public class Main {
+    public static class Product {
+        public static int product(int x, int y) {
+            return x * y;
+        }
+        public static int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public static double product(double x, double y) {
+            return x * y;
+        }
+    }
     public static void main(String[] args) {
         Product p = new Product();
         p.product(1,2);
